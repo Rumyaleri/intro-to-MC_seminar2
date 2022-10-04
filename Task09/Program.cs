4842481c7c696a71c450bca931db03c2c056455b
valeria <v.rumyantceva@gmail.com>
2022-10-04 21:01:39 +0300

    add task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -2,15 +2,27 @@
 // 1. выводит случайное число 
 
 
-int number = new Random ().Next(10, 100);
+int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из диапазона 10 - 99 => {number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-if(firstDigit == secondDigit) Console.WriteLine($"Цифры одинаковые");
-else if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа  = {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа  = {secondDigit}");
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if(firstDigit == secondDigit) Console.WriteLine($"Цифры одинаковые");
+// else if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа  = {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа  = {secondDigit}");
 
 // int res = Math.Max(firstDigit, secondDigit);
 // Console.WriteLine($"Наибольшая цифра числа  = {res}");
 
 
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    if (firstDigit > secondDigit) return firstDigit;
+    return secondDigit;
+}
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа  = {maxDigit}");
+
+//тернарный оператор = если первое число больше второго то (?) первое, иначе (:) второе
+return firstDigit > secondDigit ? firstDigit : secondDigit
