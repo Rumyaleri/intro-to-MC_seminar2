964b638a49075bafdb80cfe9eddc29d4dca0ba78
valeria <v.rumyantceva@gmail.com>
2022-10-04 21:58:32 +0300

    add task09

---
+++ Task11/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число {number}");
+int firstDigit = number / 100;
+    int secondDigit = number / 10;
+    int thirdDigit = number % 10;
+
+Console.WriteLine($"{firstDigit}{thirdDigit}");
+
+
+// int MaxDigit(int number)
+// {
+//     int firstDigit = number / 100;
+//     int secondDigit = number / 10;
+//     int thirdDigit = number % 10;
+//     if (firstDigit > secondDigit) return firstDigit;
+//     return secondDigit;
+// }
---
+++ Task12/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
