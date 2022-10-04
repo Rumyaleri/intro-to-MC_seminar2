34a7776b7ba7accc5621cb8b679bb34d831e8959
valeria <v.rumyantceva@gmail.com>
2022-10-04 20:35:00 +0300

    add task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача 9 
+// 1. выводит случайное число 
+
+
+int number = new Random ().Next(10, 100);
+Console.WriteLine($"Случайное число из диапазона 10 - 99 => {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if(firstDigit == secondDigit) Console.WriteLine($"Цифры одинаковые");
+else if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа  = {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа  = {secondDigit}");
+
+// int res = Math.Max(firstDigit, secondDigit);
+// Console.WriteLine($"Наибольшая цифра числа  = {res}");
+
+
