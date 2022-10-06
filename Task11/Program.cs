8e013d20519b4643ad754d8167cd41da77c4d3bf
valeria <v.rumyantceva@gmail.com>
2022-10-06 22:44:11 +0300

    повтор

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,17 +1,29 @@
-﻿int number = new Random().Next(100, 1000);
+﻿// задача 11.
+// Напишите программу, которая
+// 1. выводит случайное трехзначное число и 
+// 2. удаляет вторую цифру этого числа
+
+
+
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное число {number}");
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int firstThirdDigit = firstDigit * 10 + thirdDigit;
+
+// Console.WriteLine($"первая и последняя цифры числа {firstThirdDigit}");
+
+//через метод
+
+int number = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное число {number}");
-int firstDigit = number / 100;
-    int secondDigit = number / 10;
-    int thirdDigit = number % 10;
 
-Console.WriteLine($"{firstDigit}{thirdDigit}");
+int NumberPrint(int num)
+{
+   int firstDigit = number / 100;
+int thirdDigit = number % 10;
 
-
-// int MaxDigit(int number)
-// {
-//     int firstDigit = number / 100;
-//     int secondDigit = number / 10;
-//     int thirdDigit = number % 10;
-//     if (firstDigit > secondDigit) return firstDigit;
-//     return secondDigit;
-// }
+return firstDigit * 10 + thirdDigit;
+}
+int result = NumberPrint(number);
+Console.WriteLine($"{number} => {result}");
