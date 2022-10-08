6a87010700bdfc6a965489b951af9af7c2bf8821
valeria <v.rumyantceva@gmail.com>
2022-10-08 22:16:38 +0300

    повтор

---
+++ Task013/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number >= 999)
+{
+    number = number / 10;
+}
+int findDigit = number % 10;
+Console.WriteLine($"Третья цифра числа: {findDigit}");
