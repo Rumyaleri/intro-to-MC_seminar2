b2f51a31cc504e879b321fed7e9487dee1f6d7d9
valeria <v.rumyantceva@gmail.com>
2022-10-08 22:23:11 +0300

    add Task013

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -1,8 +1,13 @@
-﻿Console.Write("Введите число: ");
+﻿Console.Write("Введите положительное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99)
+{
 while (number >= 999)
 {
     number = number / 10;
 }
 int findDigit = number % 10;
 Console.WriteLine($"Третья цифра числа: {findDigit}");
+}
+else 
+Console.WriteLine($"Третья цифра отсутствует");
