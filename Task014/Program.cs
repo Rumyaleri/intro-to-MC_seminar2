7d4037c5ffdd96ed762dba5530d72c6fb3e84ddb
valeria <v.rumyantceva@gmail.com>
2022-10-08 12:58:01 +0300

    повтор

--- Task014/Program.cs
+++ Task014/Program.cs
@@ -14,17 +14,31 @@
 //     Console.WriteLine($"число {number} не кратно 7 и 23");
 
 
-bool IncomingNum(int number, int numA, int numB)
-// если в методе сразу на проверяемые числа ввести переменные, то потом в строке 26 их можно менять на любые и проводить прверку с разными числами
-{
-    return number % numA == 0 && number % numB == 0;
-}
+// bool IncomingNum(int number, int numA, int numB)
+// // если в методе сразу на проверяемые числа ввести переменные, то потом в строке 26 их можно менять на любые и проводить прверку с разными числами
+// {
+//     return number % numA == 0 && number % numB == 0;
+// }
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// bool result = IncomingNum(num, 7, 23);
+// if (result)
+//   Console.Write("да");  
+// else 
+// Console.Write("нет");
+
+// или
 
 Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
-bool result = IncomingNum(num, 7, 23);
-if (result)
-  Console.Write("да");  
+bool Remains (int a, int b)
+{
+    return a%b == 0;
+}
+if (Remains(number,7) && Remains(number,23)) 
+Console.Write("да");
 else 
 Console.Write("нет");
