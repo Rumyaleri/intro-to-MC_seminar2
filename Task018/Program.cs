5035aa50500097802f1023ba273f5734a5562e3c
valeria <v.rumyantceva@gmail.com>
2022-10-09 23:34:15 +0300

    add Task020

--- Task018/Program.cs
+++ Task018/Program.cs
@@ -4,18 +4,17 @@
 // 2. выдает диапазон возможных координат точек в этой четверти (X и Y).
 
 Console.WriteLine("Ведите номер четверти координатной плоскости: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-string result = Coordinate(number);
+string number = Console.ReadLine();
+string result = Range(number);
 
 Console.WriteLine(result);
 
-string Coordinate (int num)
+string Range (string num)
 {
-    if (num == 1) return "В четверти 1 координаты по оси X больше нуля и по оси Y больше нуля";
-    if (num == 2) return "В четверти 2 координаты по оси X меньше нуля и по оси Y больше нуля";
-    if (num == 3) return "В четверти 3 координаты по оси X меньше нуля и по оси Y меньше нуля";
-    if (num == 4) return "В четверти 4 координаты по оси X больше нуля и по оси Y меньше нуля";
+    if (num == "1") return "В четверти 1 координаты X > 0 и Y > 0";
+    if (num == "2") return "В четверти 2 координаты X < 0 и Y > 0";
+    if (num == "3") return "В четверти 3 координаты X < 0 и Y < 0";
+    if (num == "4") return "В четверти 4 координаты X > 0 и Y < 0";
     return "Некорректный ввод";
 }
 
