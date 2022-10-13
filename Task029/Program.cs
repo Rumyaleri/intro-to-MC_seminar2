972bce9c2dd4818120057f5299ec3544fdb31f25
valeria <v.rumyantceva@gmail.com>
2022-10-14 00:26:15 +0300

    add Task029

--- Task029/Program.cs
+++ Task029/Program.cs
@@ -4,5 +4,27 @@
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-int [] array = new int [] {1, 2, 3};
-Console.WriteLine("[{0}]", string.Join(", ", array));
+// int [] array = new int [] {1, 2, 3};
+// Console.WriteLine("[{0}]", string.Join(", ", array));
+
+
+
+int[] FillArray(int num)
+{
+    int[] array = new int [num];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+    }
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
+int[] myArr = FillArray(8);
+PrintArray(myArr);
